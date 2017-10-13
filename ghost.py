70b25107fd74259c3585acf99204bdffcd4dc92f
Marilyn Nowacka <marilyn.nowacka@fyber.com>
2017-10-13 14:36:00 +0200

    directions are here to stay

--- ghost.py
+++ ghost.py
@@ -1,5 +1,31 @@
 from microbit import *
+import music
+import random
 
-microbit.display.show(Image.HEART)
-microbit.display.scroll("Hello!")
-microbit.music.play(music.ENTERTAINER)
+# display.show(Image.HEART)
+# display.scroll("Hello!")
+
+
+def ghostLocation():
+    return random.randint(-10, 10), random.randint(-10, 10)
+
+
+def move(head):
+    return head
+
+
+X, Y = ghostLocation()
+compass.calibrate()
+needle = ((15 - compass.heading()) // 30) % 12
+display.show(Image.ALL_CLOCKS[needle])
+
+while True:
+    if button_a.is_pressed():
+        # needle = ((15 - compass.heading()) // 30) % 1
+        print(str(needle))
+        print()
+        display.show("S")
+    else:
+        needle = ((15 - compass.heading()) // 30) % 12
+        display.show(Image.ALL_CLOCKS[needle])
+        print()
